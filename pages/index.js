371eefc3ca894da636c3e370ccf9b204ed296d9c
Midori <82906205+midori-n@users.noreply.github.com>
2023-06-24 17:28:39 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
   return (
     <center>
       <h1>.｡.:*･ﾟ To my best friend and favorite WoL ﾟ･*:.｡.</h1>
-      <h2>I wish you a</h2>
+      <h2>- I wish you a -</h2>
       <h1>･:*:･｡(〃･ω･)ﾉ  HappyBirthdayヽ(･ω･〃)｡･:*:･ﾟ</h1>
     </center>
   )
