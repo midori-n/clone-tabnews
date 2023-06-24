826dfff30a491f9480760bab54ebbe8fa2503598
Midori <82906205+midori-n@users.noreply.github.com>
2023-06-24 18:46:53 +0000

    Adiciona nova frase

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@
 function Home() {
   return (
     <center>
-      <h1>.｡.:*･ﾟ To my best friend and favorite WoL ﾟ･*:.｡.</h1>
-      <h2>- I wish you a -</h2>
-      <h1>･:*:･｡(〃･ω･)ﾉ  HappyBirthdayヽ(･ω･〃)｡･:*:･ﾟ</h1>
+      <h2>Vou fazer essa tela desaparecer!</h2>
+      <h1>Aperte ALT+F4 e veja a mágica acontecer (◔ ౪◔)⊃━☆ﾟ.*・</h1>
     </center>
   )
 }
