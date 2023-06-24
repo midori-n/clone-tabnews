9d71abc224b6fb91d4ca442e5a07281a3c1f040c
Midori <82906205+midori-n@users.noreply.github.com>
2023-06-24 01:05:50 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <center>
+      <h1>.｡.:*･ﾟ To my best friend and favorite WoL ﾟ･*:.｡.</h1>
+      <h2>I wish you a</h2>
+      <h1>･:*:･｡(〃･ω･)ﾉ  HappyBirthdayヽ(･ω･〃)｡･:*:･ﾟ</h1>
+    </center>
+  )
+}
+
+export default Home;
